75b5455864d9ee9c919e6f0e4927ad430074fa47
Hoang Nhan <hnhan793@gmail.com>
2023-05-11 00:06:13 +0700

    push code

---
+++ main.js
@@ -0,0 +1,40 @@
+const bmiHeight = document.querySelector('#bmi__height');
+const bmiWeight = document.querySelector('#bmi__weight');
+const btnResult = document.querySelector('#btn__result');
+const outputArea = document.querySelector('#output__area');
+
+function calcBMI(height, weight) {
+    return weight / (Math.pow((height / 100), 2));
+}
+
+btnResult.addEventListener('click', function (e) {
+    e.preventDefault();
+    let result = calcBMI(bmiHeight.value, bmiWeight.value);
+    console.log(result);
+    if (result < 18.5) {
+        console.log('vap if');
+        outputArea.innerHTML = `
+            <img src="./img/body-1.png" alt="img" class="w-100">
+            <p class="mb-0 text-white">Under weight : ${result.toFixed(2)}</p>
+        `;
+    }
+    else if (result >= 18.5 && result <= 24.9) {
+        outputArea.innerHTML = `
+            <img src="./img/body-2.png" alt="img" class="w-100">
+            <p class="mb-0 text-white">Normal : ${result.toFixed(2)}</p>
+        `;
+    }
+    else if (result >= 25 && result <= 29.9) {
+        outputArea.innerHTML = `
+            <img src="./img/body-3.png" alt="img" class="w-100">
+            <p class="mb-0 text-white">Over weight : ${result.toFixed(2)}</p>
+        `;
+    }
+    else {
+        outputArea.innerHTML = `
+            <img src="./img/body-4.png" alt="img" class="w-100">
+            <p class="mb-0 text-white">Severe obesity : ${result.toFixed(2)}</p>
+        `;
+    }
+})
+
